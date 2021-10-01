544e347416fcdb19bec06830a1519a71a3207cfc
Unnati Mishra <68197005+unnati-m@users.noreply.github.com>
2021-10-01 14:50:18 +0530

    Create client.c

---
+++ udp_echo/client.c
@@ -0,0 +1,48 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main() {
+	const char* server_name = "localhost";
+	const int server_port = 8877;
+
+	struct sockaddr_in server_address;
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+
+	// creates binary representation of server name
+	// and stores it as sin_addr
+	// http://beej.us/guide/bgnet/output/html/multipage/inet_ntopman.html
+	inet_pton(AF_INET, server_name, &server_address.sin_addr);
+
+	// htons: port in network order format
+	server_address.sin_port = htons(server_port);
+
+	// open socket
+	int sock;
+	if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+		printf("could not create socket\n");
+		return 1;
+	}
+
+	// data that will be sent to the server
+	const char* data_to_send = "Gangadhar Hi Shaktimaan hai";
+
+	// send data
+	int len =
+	    sendto(sock, data_to_send, strlen(data_to_send), 0,
+	           (struct sockaddr*)&server_address, sizeof(server_address));
+
+	// received echoed data back
+	char buffer[100];
+	recvfrom(sock, buffer, len, 0, NULL, NULL);
+
+	buffer[len] = '\0';
+	printf("recieved: '%s'\n", buffer);
+
+	// close the socket
+	close(sock);
+	return 0;
+}
